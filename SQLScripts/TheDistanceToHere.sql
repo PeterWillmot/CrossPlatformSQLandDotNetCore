84c8f979d642a7750099ca29756658bbdee9a7e6
Peter Willmot <peter@Qri.ie>
2019-03-10 10:34:46 +0000

    Initial

---
+++ SQLScripts/TheDistanceToHere.sql
@@ -0,0 +1,28 @@
+USE GeoDemo
+GO
+
+IF NOT EXISTS(SELECT 1 FROM QRIGeoSpatialInfoSource.dbo.Airports WHERE city = 'Reykjavik') INSERT QRIGeoSpatialInfoSource.dbo.Airports(city, lon, lat) VALUES('Reykjavik', -22.625723, 63.998562)
+
+SELECT * FROM QRIGeoSpatialInfoSource.dbo.Airports
+
+IF EXISTS(SELECT 1 FROM sys.objects WHERE name = 'Airports') DROP TABLE Airports
+GO
+
+CREATE TABLE Airports
+(
+	city VARCHAR(MAX)
+	, location GEOGRAPHY
+)
+GO
+
+INSERT Airports(city, location)
+SELECT city, 'POINT(' + CAST(lon AS VARCHAR(MAX)) + ' ' + CAST(lat AS VARCHAR(MAX)) + ')'
+FROM QRIGeoSpatialInfoSource.dbo.Airports
+
+
+DECLARE @reyk GEOGRAPHY
+SELECT @reyk = location FROM Airports WHERE city = 'Reykjavik'
+
+SELECT city, CAST((location.STDistance(@reyk) / 1000) AS INTEGER) AS 'KMs'
+FROM Airports
+WHERE city <> 'Reykjavik'
---
+++ SimpleConsoleADO/SimpleConsoleADO/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace SImpleConsoleADO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (SqlConnection sqlConn = new SqlConnection("Data Source = localhost; Initial Catalog = GeoDemo; Integrated Security = True"))
+                {
+                    sqlConn.Open();
+
+                    using (SqlCommand cmd = sqlConn.CreateCommand())
+                    {
+                        cmd.CommandText = "SELECT city FROM Airports";
+
+                        SqlDataReader rdr = cmd.ExecuteReader();
+
+                        while(rdr.Read())
+                        {
+                            Console.WriteLine(String.Format("City: {0}", rdr["City"]));
+                        }
+                    }
+                }
+            }
+
+            catch (Exception err)
+            {
+                Console.WriteLine("ERROR: {0}", err.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
